c8108768b4253b49de787f0af88d259f64dadec5
saputra164 <35791548+saputra164@users.noreply.github.com>
2018-08-27 09:34:25 +0700

    sad
    gagal

---
+++ basetileset.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="basetileset" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
+ <image source="Assets/art/roguelikeSheet_transparent.png" width="968" height="526"/>
+ <terraintypes>
+  <terrain name="dirt" tile="578"/>
+ </terraintypes>
+ <tile id="291">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.0909091" width="16" height="15.8182"/>
+  </objectgroup>
+ </tile>
+ <tile id="458" terrain="0,0,0,0"/>
+ <tile id="459" terrain="0,0,0,0">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0909091" y="0.181818" width="16" height="15.7273"/>
+  </objectgroup>
+ </tile>
+ <tile id="460" terrain="0,0,0,0"/>
+</tileset>
